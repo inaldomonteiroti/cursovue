65522cc8075e000dea950b6f302de88a7201a529
inaldo.monteiro <inaldo.monteiro@adagri.ce.gov.br>
2022-08-01 11:39:27 -0300

    Ajustar

--- 1 instalacao/src/app.js
+++ 1 instalacao/src/app.js
@@ -1,10 +1,23 @@
 const MyNameApp = {
     data(){
         return{
-            name:"Matheus",
-            age: 30
+            name:"",
+            age: 30,
+            input_name:""
+        }
+    },
+    methods:{
+        submitForm(e){
+
+            e.preventDefault();
+
+            console.log("Caiu aqui");
+
+            // console.log(this.input_name);
+
+            // this.name = this.input_name;
+
         }
     }
 }
-
-Vue.createApp(MyNameApp).mount("#app")
+Vue.createApp(MyNameApp).mount("#app");
