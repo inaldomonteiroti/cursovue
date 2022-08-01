a1d139cca5c00d8036856538d98a3f5fd9270cbd
inaldo.monteiro <inaldo.monteiro@adagri.ce.gov.br>
2022-08-01 16:35:52 -0300

    Ajustes

---
+++ instalacao2/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <PrimeiroComponente /><br>
+  <primeiro-componente/>
+</template>
+
+<script>
+
+import PrimeiroComponente from './components/PrimeiroComponente.vue'
+
+  export default{
+    name: 'App',
+    components:{
+      PrimeiroComponente
+    }
+  }
+
+</script>
---
+++ instalacao2/src/components/PrimeiroComponente.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Olá VUE</h1>
+</template>
+
+<script>
+
+export default{
+    name:'PrimeiroComponente'
+}
+</script>
